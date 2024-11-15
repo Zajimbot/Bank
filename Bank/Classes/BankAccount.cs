9208b87e07d034f205bc9c2db9a618237d34e74e
Artur <Karturokga@gmail.com>
2024-11-15 20:17:32 +0300

    1

---
+++ Bank/Classes/BankAccount.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Classes
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum StatusS
+    {
+        open, //открыт 
+        сlosed, // закрыт 
+        bankrupt //банкрот
+
+    }
+
+    public class BankAccount
+    {
+
+        public int accountNumber { get; set; } // номер счета
+        public DateTime dateOpen { get; set; } // Дата открытия счета
+        public double moneyAccount { get; set; } //Сумма на счету 
+        public DateTime depositOpen { get; set; } // Дата открытия вклада
+        public int depositPeriod { get; set; } // Срок вклада в месецах
+        public string status { get; set; } //Статус 
+
+        private DateTime DepositClose()  // Дата окончания
+        {
+            DateTime close;
+            close = this.depositOpen.AddDays(this.depositPeriod);
+            return close;
+        }
+        private void СhangeStatus()  // Изменение статуса
+        {
+            if (this.moneyAccount > 0)
+                status = Convert.ToString(StatusS.open);
+            if (this.moneyAccount < 0)
+                status = Convert.ToString(StatusS.bankrupt);
+        }
+        public void InputMani(double input, bool inputOutput) // Внесение снятие 
+        {
+            if (inputOutput)
+            {
+                this.moneyAccount += input;
+            }
+            else
+            {
+                if( this.moneyAccount > input)
+                this.moneyAccount -= input;
+            }
+        }
+        public void Translation ()
+        {
+
+        }
+
+
+
+        public BankAccount()
+        {
+            this.accountNumber = 0;
+            this.dateOpen = DateTime.Now;
+            this.moneyAccount = 0;
+            this.depositPeriod = 0;
+            this.status = Convert.ToString( StatusS.сlosed);
+        }
+        public BankAccount(int accountNumber, DateTime dateOpen, double moneyAccount, DateTime depositOpen, int depositPeriod, StatusS status)
+        {
+            this.accountNumber = accountNumber;
+            this.dateOpen = dateOpen;
+            this.moneyAccount = moneyAccount;
+            this.depositOpen = depositOpen;
+            this.depositPeriod = depositPeriod;
+            this.status = Convert.ToString( status);
+        }
+    }
+}
---
+++ Bank/Classes/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Classes
+{
+    internal class Client
+    {
+        public String name { get; set; }
+        public String surname { get; set; }
+        public String middleName { get; set; }
+        public int passportNumber { get; set; }
+        public DateTime birthВate { get; set; }
+        public Client()
+        {
+            this.name = "";
+            this.surname = "";
+            this.middleName = "";
+            this.passportNumber = 0;
+            this.birthВate = DateTime.Now;
+        }
+        public Client(string name, string surname, string middleName, int passportNumber, DateTime birthВate)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.middleName = middleName;
+            this.passportNumber = passportNumber;
+            this.birthВate = birthВate;
+        }
+    }
+    
+}
